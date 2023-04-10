21af1d45be51a9f891590723cd84d613ce20686b
Evripidis619 <euripo2000@gmail.com>
2023-04-10 19:25:02 +0300

    End of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -14,13 +17,16 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     BsDropdownModule.forRoot()
 
     
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav',
@@ -8,23 +12,28 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any ={};
-  loggedIn=false;
-
-  constructor(private accountService:AccountService) { }
+  currentUser$:Observable<User|null>=of(null);
+  
+  constructor(public accountService:AccountService) { }
 
   ngOnInit(): void {
+    this.currentUser$=this.accountService.currentUser$;
   }
+ 
+
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
+        
+        
       }, 
       error : error=>console.log(error)
     })
   }
   logout(){
-    this.loggedIn=false;
+    this.accountService.logout();
+    
   }
 
 }
